cde98e248a33af40e0192b09a76653f16bf27ed4
Bruce Chung <brucekchung@gmail.com>
2017-10-21 17:41:09 -0600

    Obligatory commit

--- script.js
+++ script.js
@@ -16,32 +16,13 @@ loadIdeas();
 function submitIdea(event) {
   event.preventDefault();
   prepend(titleInput.val(), bodyInput.val())
-  // ideaStorage.prepend(
-  //   ` 
-  //   <article class="card">
-  //   <h2 class="idea-title"> ${titleInput.val()}
-  //   <span class="delete">
-  //   <img src="images/delete.svg" class="delete" alt="delete-icon">
-  //   </span>
-  //   </h2>
-  //   <p class="idea-body">${bodyInput.val()}</p>
-  //   <span class="upvote">
-  //   <img src="images/upvote.svg" alt="upvote-icon">
-  //   </span>
-  //   <span class="downvote">
-  //   <img src="images/downvote.svg" alt="downvote-icon">
-  //   </span>
-  //   <p class="bold-quality-text">quality: <span class="quality">swill</span></p>
-  //   </article>
-  //   `
-  //   );
 };
 
 function prepend(title, body) {
     ideaStorage.prepend(
     ` 
     <article class="card">
-    <h2 class="idea-title"> ${title}
+    <h2 class="idea-title" contenteditable="true"> ${title}
     <span class="delete">
     <img src="images/delete.svg" class="delete" alt="delete-icon">
     </span>
@@ -69,25 +50,6 @@ function addStorage() {
 function loadIdeas() {
   for (var i=0; i<Object.keys(localStorage).length; i++) {
     prepend(JSON.parse(Object.values(localStorage)[i]).title, JSON.parse(Object.values(localStorage)[i]).body)
-    // ideaStorage.prepend(
-    // ` 
-    // <article class="card">
-    // <h2 class="idea-title"> ${JSON.parse(Object.values(localStorage)[i]).title}
-    // <span class="delete">
-    // <img src="images/delete.svg" class="delete" alt="delete-icon">
-    // </span>
-    // </h2>
-    // <p class="idea-body">${JSON.parse(Object.values(localStorage)[i]).body}</p>
-    // <span class="upvote">
-    // <img src="images/upvote.svg" alt="upvote-icon">
-    // </span>
-    // <span class="downvote">
-    // <img src="images/downvote.svg" alt="downvote-icon">
-    // </span>
-    // <p class="bold-quality-text">quality: <span class="quality">swill</span></p>
-    // </article>
-    // `
-    // );
   }
 }
 
